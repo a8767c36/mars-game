583ee647ad8d64ba0c6356735346549a27268990
Lorenzo Tobias Ferrara <csaw9203@student.uibk.ac.at>
2023-08-07 00:17:00 +0200

    mars map

--- scene/mars-map.js
+++ scene/mars-map.js
@@ -1,8 +1,15 @@
 
 export const map = { };
 map["0,0"] = "garden";
-map["0,1"] = "garden";
-map["0,2"] = "garden";
+map["1,0"] = "garden";
+map["2,0"] = "garden";
+
+const cost = {
+	"water-tank": {
+		"bricks": 20,
+		"plastics": 10,
+	},
+};
 
 export
 async function show () {
@@ -15,7 +22,16 @@ async function show () {
 	// draw the biomes.
 	drawBiomes();
 
+	// hide over view
+	$("#over").css("display", "none");
+	$("#close").on("click", function() {
+		changeScene("mars-map");
+	})
+
 	// install the onclick handler
+	var r = null;
+	var biome = null;
+
 	$(ctx.canvas).on("click", function (e) {
 		// first, find out the biome that was clicked on.
 		var offX  = (e.offsetX || e.pageX - $(e.target).offset().left);
@@ -24,6 +40,8 @@ async function show () {
 		var i = Math.floor(offX / width  * 4);
 		var j = Math.floor(offY / height * 4);
 		console.log("i j", i, j);
+		r = `${i},${j}`;
+		biome = map[r];
 
 		// highlight the chosen biome a bit.
 		drawBackground();
@@ -31,7 +49,43 @@ async function show () {
 		ctx.strokeRect(160*i + 2, 160*j + 2, 156, 156);
 
 		// display information about the biome.
+		// in a modal window
+		$("#over")
+			.css("display", "block");
+		$("#info")
+			.html(`Ort: ${r}\nBiome: ${biome || "Empty"}\n\n&nbsp;`);
+		if (biome) {
+			$('#build')
+				.css("display", "none");
+			$('#reset')
+				.css("display", "block")
+				.on("click", function() {
+
+				});
+		} else {
+			$('#reset')
+				.css("display", "none");
+			$('#build')
+				.css("display", "block")
+		}
 	})
+	// install onclick handlers for the over view
+	$("#reset").on("click", function() {
+		if (confirm("Reset biome?"))
+			map[r] = null;
+			changeScene("mars-map");
+	})
+	$("#build select").on("change", function(e) {
+		console.log("Change", e.target.value);
+		$("#build-cost").text(`Will cost:\n`);
+		var c = cost[e.target.value] || { };
+		for (let i in c) {
+			$("#build-cost").append(`${i}: ${c[i]}\n`);
+		}
+	});
+	$("#build input[type=button]").on("click", function() {
+		console.log("Selected value:", $("#build select").val());
+	});
 }
 
 function drawBackground() {
