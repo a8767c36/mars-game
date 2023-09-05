3f91712ae1c0e0535da01adc0f537a875624a1f6
Lorenzo Ferrara <lorenzo.ferrara@gmx.at>
2023-09-06 01:55:51 +0200

    chem lager

--- scene/chem.js
+++ scene/chem.js
@@ -1,2 +1,29 @@
-// ...
+
+const shelf = {
+	h2: 1,
+	gas: 1,
+}
+
+export
+const data = {
+	shelf: shelf
+}
+
+export
+function show () {
+	// programmatische darstellung des lagers
+
+	for (let div of $("#shelf .chem")) {
+		let text = div.innerText;
+		let cls  = $(div).data("chem");
+		$(div).empty();
+		$(div).append(
+			$('<span class="icon"></span>'),
+			$('<span class="desc"></span>').append(
+				$('<span class="title"></span>').text(text),
+				$('<span class="info"></span>').text(`Lagernd: ${shelf[cls] || 0} kg`)
+			)
+		);
+	}
+}
 
