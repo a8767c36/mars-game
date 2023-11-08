234e717ce09451ca24d3c063d3ddd27b9f5e3200
Lorenzo Ferrara <lorenzo.ferrara@gmx.at>
2023-11-08 19:32:49 +0100

    chem ..

--- scene/chem.js
+++ scene/chem.js
@@ -16,7 +16,7 @@ function show () {
 		let cls  = $(div).data("chem");
 		$(div).empty();
 		$(div).append(
-			$('<span class="icon"></span>'),
+			$('<span class="icon"></span>').toggleClass("full", shelf[cls] ? true : false),
 			$('<span class="desc"></span>').append(
 				$('<span class="title"></span>').text(text),
 				$('<span class="info"></span>').text(`Lagernd: ${shelf[cls] || 0} kg`)
